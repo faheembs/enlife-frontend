2ed9ff082098021a5c4f4e585dfb7e65093fa815
Rana Ammar Talat <faheembuildout@gmail.com>
2024-05-21 15:20:04 +0500

    Updated Url

--- src/Utils/urls.ts
+++ src/Utils/urls.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000/";
-
+// export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://mauve-goldfish-cape.cyclic.app/";
 // Endpoints
 export const ENDPOINTS = {
   LOGIN: "auth/login",
